73490f1542b5cf8e1e9b92b8b10d827207590882
chamankdas <chamandas254@gmail.com>
2026-08-24 12:54:10 +0530

    feat: add various agent files for code review and debugging guidance

--- tsm/tsm/settings.py
+++ tsm/tsm/settings.py
@@ -53,7 +53,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "inertia.middleware.InertiaMiddleware",
 ]
 
 ROOT_URLCONF = 'tsm.urls'
